adf200c1e55bb655feb089faa5d18a0396b69436
vibha2 <juhisahu2203@gmail.com>
2024-01-13 20:02:24 +0530

    api connector created

--- src/components/common/Navbar.jsx
+++ src/components/common/Navbar.jsx
@@ -1,12 +1,40 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import logo from "../../assets/Logo/Logo-Full-Light.png"
 import { Link, matchPath } from 'react-router-dom'
 import {NavbarLinks} from "../../data/navbar-links"
 import { useLocation } from 'react-router-dom'
-
+import { useSelector } from 'react-redux';
+import {AiOutlineShoppingCart} from 'react-icons/ai';
+import ProfileDropDown from '../core/Auth/ProfileDropDown';
+import { apiConnector } from '../../services/apiconnector'
+import { categories } from '../../services/api';
+import {useState} from 'react';
 
 function Navbar() {
+
+    const {token} = useSelector( (state) => state.auth );
+    const {user} = useSelector( (state) => state.profile );
+    const {totalItems} = useSelector( (state) => state.cart);
+
     const location = useLocation();
+
+    const [subLinks, setSubLinks] = useState([]);
+
+    const fetchSublinks = async() => {
+        try{
+            const result = await apiConnector("GET", categories.CATEGORIES_API);
+            console.log("printing sublinks result: ", result);
+
+            setSubLinks(result.data.data);
+        }
+        catch(error){
+            console.log("Could not fetch the category list")
+        }
+     }
+
+    useEffect( () => {
+        fetchSublinks();
+    }, [])
 
     const matchRoute = (route) => {
         // jo bhi path click krre hai i.e path:route equal hota hai url le path se i.e location.pathname
@@ -29,7 +57,9 @@ function Navbar() {
                         <li key={index}>
                         {
                             link.title === "Catalog"?
-                            (<div>abc</div>):
+                            (
+                                <div></div>
+                            ):
                             (
                             <Link to={link?.path}>
                              <p className={`${matchRoute(link?.path)? "text-yellow-25": "text-richblack-25"}`}>{link.title}</p>
@@ -48,7 +78,44 @@ function Navbar() {
             {/* Buttons */}
             {/* Login/Signup/Dashboard */}
             <div className='flex gap-x-4 items-center'>
-                
+                {
+                    user && user?.accountType != "Instructor" && (
+                        <Link to="/dashboard/cart" className='relative'>
+                            <AiOutlineShoppingCart />
+                            {
+                                totalItems > 0 && (
+                                    <span>
+                                        {totalItems}
+                                    </span>
+                                )
+                            }
+                        </Link>
+                    )
+                }
+                {
+                    token === null && (
+                        <Link to="/login">
+                            <button className='border border-richblack-700 bg-richblack-800 px-[12px] py-[8px] text-richblack-100 rounded-md'>
+                                Log in
+                            </button>
+                        </Link>
+                        
+                    )
+                }
+                {
+                    token === null && (
+                        <Link to="/signup">
+                            <button className='border border-richblack-700 bg-richblack-800 px-[12px] py-[8px] text-richblack-100 rounded-md'>
+                                Sign Up
+                            </button>
+                        </Link>
+                    )
+                }
+                {
+                    token !=null && (
+                        <ProfileDropDown />
+                    )
+                }
             </div>
         </div>
     </div>
